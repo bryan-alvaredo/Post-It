44389e6adb85c49ac7826d6fc91683020a2a4ac2
Bryan <bryan.alvaredo95@gmail.com>
2022-04-26 18:41:55 -0300

    primer commit del proyecto

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const inputFormulario = document.querySelector("#formulario");
+const seccionNota = document.querySelector("#seccion");
+let contadorId = 0;
+
+inputFormulario.addEventListener("submit",e => {
+    e.preventDefault();
+    console.log(e);
+    crearNotita(e.target[0].value, e.target[1].value);
+})
+
+const crearNotita = (texto,color) => {
+    contadorId++;
+    console.log(contadorId);
+    const elementoNuevo = document.createElement("article");
+    elementoNuevo.id = contadorId;
+    elementoNuevo.classList.add("notita");
+    elementoNuevo.classList.add(color);
+    elementoNuevo.innerHTML = `<strong onclick="borrarNotita(${contadorId})">X</strong><p> ${texto} </p>`;
+    seccionNota.appendChild(elementoNuevo);
+}
+
+const borrarNotita = (idX) => {
+    const notitaBorrar = document.getElementById(`${idX}`);
+    seccionNota.removeChild(notitaBorrar);
+}
